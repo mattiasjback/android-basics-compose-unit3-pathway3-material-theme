0a1f3c839600fbd3b2b789d312d29530875381b7
Mattias Bäck <mattias.back@bontouch.com>
2022-09-29 10:00:42 +0200

    Apply spring animation to expand toggle

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -20,6 +20,9 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
@@ -89,7 +92,14 @@ fun DogItem(dog: Dog, modifier: Modifier = Modifier) {
             .padding(8.dp),
         elevation = 4.dp
     ) {
-        Column() {
+        Column(
+            modifier = Modifier.animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessMediumLow
+                )
+            )
+        ) {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
